da1640a5d51cb98ce301a188b69b5f82647c1d69
Unknown <justataya@gmail.com>
2018-10-07 13:54:49 +0300

    IPv4 & IPv6 structures

--- Lab1/IPAddress.cpp
+++ Lab1/IPAddress.cpp
@@ -1,79 +1,76 @@
 //
 // Created by taya on 02.10.18.
 //
-/*
+
+
+
 #include "IPAddress.h"
 
-std::bitset<32> &IP4::returnBinary() {
-    //   std::bitset<32> binary();
+IP4::IP4(unsigned int *ip) {
+    for (size_t i = 0; i < 4; i++)
+        octet[i] = ip[i];
 }
 
-
-const char *hex_to_bin(char c) {
-    switch (toupper(c)) {
-        case '0':
-            return "0000";
-        case '1':
-            return "0001";
-        case '2':
-            return "0010";
-        case '3':
-            return "0011";
-        case '4':
-            return "0100";
-        case '5':
-            return "0101";
-        case '6':
-            return "0110";
-        case '7':
-            return "0111";
-        case '8':
-            return "1000";
-        case '9':
-            return "1001";
-        case 'A':
-            return "1010";
-        case 'B':
-            return "1011";
-        case 'C':
-            return "1100";
-        case 'D':
-            return "1101";
-        case 'E':
-            return "1110";
-        case 'F':
-            return "1111";
+std::bitset<32> IP4::getBinary() {
+    std::bitset<32> binary;
+    int it = 0;
+    int tmp = 0;
+    for (int i = 3; i >= 0; i--) {
+        tmp = this->octet[i];
+        for (int j = 0; j < 8; j++) {
+            if (tmp % 2 == 0)
+                binary[it] = 0;
+            else
+                binary[it] = 1;
+            it++;
+            tmp /= 2;
+        }
     }
+    return binary;
 }
 
-//TODO: test if it works
-std::bitset<128> &IP6::returnBinary() {
+
+IP6::IP6(std::string *ip) {
+    for (size_t i = 0; i < 8; i++)
+        octet[i] = ip[i];
+}
+
+std::bitset<128> IP6::getBinary() {
     std::bitset<128> binary;
-    size_t octet_size = 0;
-    const char *bin;
-    size_t i = 127;
-    for (size_t j = 7; j >= 0; j--) {
-        octet_size = this->octet[j].length();
-        for (size_t k = octet_size - 1; k >= 0; k--) {
-            bin = hex_to_bin(this->octet[j][k]);
-            for (size_t p = 3; p <= 0; p--) {
-                binary[i] = bin[p];
-                i--;
+    int it=0;
+    std::string tmp,buff;
+    transform();
+    for(int i=7;i>=0;i--)
+    {
+        tmp=this->octet[i];
+        for(int j=3;j>=0;j--)
+        {
+            buff=HextoBin[tmp[j]];
+            for(int k=3;k>=0;k--)
+            {
+                if(buff[k]=='0')
+                    binary[it]=0;
+                else
+                    binary[it]=1;
+                it++;
             }
         }
-        for (size_t k = 4 - octet_size; k >= 0; k--)
-            for (size_t p = 3; p <= 0; p--) {
-                binary[i] = '0';
-                i--;
-            }
     }
-    std::cout << binary << std::endl;
+    compress();
+    return binary;
 }
 
-
-void SubNet::converttoIP6() {
-    address.converttoIP6();
-    mask = 96 + mask;   //128-(32-mask) ipv4 mask is 32-bit, ipv6 mask is 128-bit
+void IP6::transform() {
+    for (size_t i = 0; i < 8; i++) {
+        if (octet[i].length() < 4)
+            while (octet[i].length() < 4)
+                octet[i].insert(0, "0");
+   }
 }
 
-*/
+void IP6::compress() {
+    for (size_t i = 0; i < 8; i++) {
+        while (octet[i][0] == '0')
+            octet[i].erase(0, 1);
+    }
+}
--- Lab1/IPAddress.h
+++ Lab1/IPAddress.h
@@ -4,67 +4,31 @@
 
 #ifndef LAB1_1_IPADRESS_H
 #define LAB1_1_IPADRESS_H
-/*
+
+#include "Convert.h"
 #include <iostream>
 #include <bitset>
-#include <map>
-#include <ctime>
-#include <random>
 
-const char *hex_to_bin(char c);
-
-std::string getBin(unsigned dec);
-
-//flag to know which ip protocol version is used
-enum IPVersion {
+enum IPversion {
     v4, v6
 };
 
 struct IP4 {
-    unsigned octet[4];  //4 decimal numbers
-    std::bitset<32> &returnBinary();
+    explicit IP4(unsigned ip[4]);
+
+    unsigned octet[4];  //4 decimal numbers from 0 to 255
+    std::bitset<32> getBinary();   //IPv4 in binary form
 };
 
 struct IP6 {
+    explicit IP6(std::string *ip);
+
     std::string octet[8];
 
-    std::bitset<128> &returnBinary();
+    std::bitset<128> getBinary();
+
+    void transform();    //add omitted zeros
+    void compress();    //zeros can be omitted
 };
 
-
-//class to represent IP address
-struct IPAddress {
-    explicit IPAddress() : ipv4(nullptr), ipv6(nullptr) { version = v4; };
-
-    explicit IPAddress(const unsigned* v4_input[4]):ipv4();
-
-    explicit IPAddress(const std::string* v6_input[8]):;
-
-    explicit IPAddress(IP4* address):ipv4(address){version=v4;};
-
-    explicit IPAddress(IP6* address):ipv6(address){version=v6};
-
-    IPVersion version;
-    IP4 *ipv4;
-    IP6 *ipv6;
-
-    void setRandomIP4();
-
-    void setRandomIP6();
-
-//  void converttoIP4();
-
-    void converttoIP6();
-};
-
-//CIDR: {IPv4 or IPv6}/{subnet mask}
-struct SubNet {
-    explicit SubNet(IP4* address, unsigned mask) : address(IPAddress(address)), mask(mask) {};
-
-    explicit SubNet(IP6* address, unsigned mask) : address(IPAddress(address)), mask(mask) {};
-    IPAddress address;
-    unsigned mask;  //subnet mask
-    void converttoIP6();
-};
-*/
 #endif //LAB1_1_IPADRESS_H
