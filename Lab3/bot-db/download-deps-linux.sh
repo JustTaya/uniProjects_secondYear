bc2698b0fbca28947ae22ab7e0ee038fb6d5dee2
Unknown <justataya@gmail.com>
2018-12-12 17:43:18 +0200

    Fixed bash scripts.

--- Lab3/bot-db/download-deps-linux.sh
+++ Lab3/bot-db/download-deps-linux.sh
@@ -10,7 +10,15 @@
 
 function downloadDeps {
     sudo apt-get update && sudo apt-get -y upgrade
+    sudo apt-get install python3
     sudo apt-get install python-pip
+    sudo apt-get install postgresql
+    sudo -u postgres createuser user
+    sudo -u postgres createdb db
+    sudo -u postgres psql
+    alter user user with encrypted password 'postgres';
+    grant all privileges on database db to user;
+    \q
     pip install -r requirements.txt --no-index --find-links file:///tmp/packages
     exit
 }
--- Lab3/bot-db/download-deps-windows.sh
+++ Lab3/bot-db/download-deps-windows.sh
@@ -10,6 +10,12 @@
 
 function downloadDeps {
     python get-pip.py
+    psql -u postgres
+    CREATE ROLE user LOGIN PASSWORD postgres;
+    CREATE DATABASE db;
+    ALTER DATABASE db OWNER TO user;
+    \q
+    psql -u myusername -d myowndb
     pip install -r requirements.txt --no-index --find-links file:///tmp/packages
     exit
 }
